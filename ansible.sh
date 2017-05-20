35dffbedb8d5fb74802b7463cd618189f23bd2da
Manoj Sharan <gms.94@icloud.com>
2017-05-19 23:18:50 -0400

    Fixing incorrect reference

--- ansible.sh
+++ ansible.sh
@@ -10,4 +10,4 @@ git clone https://github.com/gms298/Configuration-Management-using-Ansible
 cd Configuration-Management-using-Ansible/
 
 echo "RUNNING ANSIBLE PLAYBOOK"
-ansible-playbook playbook.yml -s -vvvv
+ansible-playbook main.yml -s -vvvv
